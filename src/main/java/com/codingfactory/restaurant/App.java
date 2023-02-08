dbe85248b3a1af60c83ba99f334ad07f6216a2be
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-02-08 16:17:34 +0100

    feat(router): Pass the app class to the factory controller

--- src/main/java/com/codingfactory/restaurant/App.java
+++ src/main/java/com/codingfactory/restaurant/App.java
@@ -1,9 +1,11 @@
 package com.codingfactory.restaurant;
 
+import com.codingfactory.restaurant.controllers.FactoryController;
 import com.mongodb.client.MongoDatabase;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
@@ -13,10 +15,14 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("views/layout.fxml"));
+        FXMLLoader layoutLoader = new FXMLLoader(App.class.getResource("views/layout.fxml"));
+        Parent layout = layoutLoader.load();
+        FactoryController factoryController = layoutLoader.getController();
+        factoryController.setApp(this);
+
         Screen screen = Screen.getPrimary();
         Rectangle2D bounds = screen.getVisualBounds();
-        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(layout);
 
         // Set the stage to be the size of the screen
         stage.setX(bounds.getMinX());
@@ -27,6 +33,7 @@ public class App extends Application {
         stage.setTitle("El Ristorante");
         stage.setScene(scene);
         stage.show();
+
         // database use Connection
         MongoDatabase db = MongoConnection.getDatabase();
     }
