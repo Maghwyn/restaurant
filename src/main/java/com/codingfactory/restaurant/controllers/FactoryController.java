14184f18b7aa9c8ae442a672f2f90f7d1035eb44
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-02-08 16:16:51 +0100

    feat(drawer): Create a routing logic for the fxml components

--- src/main/java/com/codingfactory/restaurant/controllers/FactoryController.java
+++ src/main/java/com/codingfactory/restaurant/controllers/FactoryController.java
@@ -1,35 +1,43 @@
 package com.codingfactory.restaurant.controllers;
 
+import com.codingfactory.restaurant.App;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class ControllerFactory implements Initializable {
+public class FactoryController implements Initializable {
     @FXML
-    private GridPane view;
+    private VBox view;
 
     @FXML
-    private ControllerDrawer controllerDrawer;
+    private DrawerController drawerController;
+
+    private App app;
 
     public void setIncludedFXML(String fxmlFile) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
-            Parent root = loader.load();
+            FXMLLoader viewLoader = new FXMLLoader(app.getClass().getResource(fxmlFile));
+            Parent root = viewLoader.load();
             view.getChildren().clear();
-            view.add(root, 0, 0);
+            view.getChildren().add(root);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public void setApp(App app) {
+        this.app = app;
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        controllerDrawer.setFactoryController(this);
+        drawerController.setFactoryController(this);
     }
 }
