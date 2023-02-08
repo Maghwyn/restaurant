bdc20a116f466f7c49a64e3d5d66aee28ed97ab5
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-02-08 21:00:01 +0100

    feat(interface): Define an interface to force the class to have the factory setter method when necessary

---
+++ src/main/java/com/codingfactory/restaurant/interfaces/FactoryInterface.java
@@ -0,0 +1,7 @@
+package com.codingfactory.restaurant.interfaces;
+
+import com.codingfactory.restaurant.controllers.FactoryController;
+
+public interface FactoryInterface {
+    void setFactoryController(FactoryController controller);
+}
