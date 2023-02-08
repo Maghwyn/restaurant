a463651801145402fbddabd5e353730c589e0551
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-02-08 20:57:25 +0100

    feat(modal): Define the modal component and it's controller with the css aswell

---
+++ src/main/java/com/codingfactory/restaurant/controllers/ModalController.java
@@ -0,0 +1,41 @@
+package com.codingfactory.restaurant.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ModalController implements Initializable {
+    @FXML
+    private AnchorPane modal;
+
+    @FXML
+    private HBox modalBackground;
+
+    @FXML
+    private Button modalCloseButton;
+
+    @FXML
+    private HBox modalContent;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        modalCloseButton.setOnMouseClicked(this::closeModal);
+    }
+
+    private void closeModal(MouseEvent e) {
+        modal.setVisible(false);
+        modalContent.getChildren().clear();
+    }
+
+    public void openModal(Parent root) {
+        modal.setVisible(true);
+        modalContent.getChildren().add(root);
+    }
+}
