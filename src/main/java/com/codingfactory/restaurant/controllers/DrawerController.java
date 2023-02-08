9e128b0c3f8c02ea2ee45de382fd932916b0ee46
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2023-02-08 16:16:51 +0100

    feat(drawer): Create a routing logic for the fxml components

---
+++ src/main/java/com/codingfactory/restaurant/controllers/DrawerController.java
@@ -0,0 +1,116 @@
+package com.codingfactory.restaurant.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+enum Target {
+    MENU,
+    TABLES,
+    COMMANDS,
+    EMPLOYEES,
+    REPORT,
+}
+
+enum State {
+    OPEN,
+    CLOSED,
+}
+
+public class DrawerController implements Initializable {
+    @FXML
+    private GridPane drawer;
+
+    @FXML
+    private Button drawerToggle;
+
+    @FXML
+    private HBox menu;
+
+    @FXML
+    private HBox tables;
+
+    @FXML
+    private HBox commands;
+
+    @FXML
+    private HBox employees;
+
+    @FXML
+    private HBox report;
+
+    private FactoryController factoryController;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        drawerToggle.getProperties().put("state", State.OPEN);
+        menu.getProperties().put("target", Target.MENU);
+        tables.getProperties().put("target", Target.TABLES);
+        commands.getProperties().put("target", Target.COMMANDS);
+        employees.getProperties().put("target", Target.EMPLOYEES);
+        report.getProperties().put("target", Target.REPORT);
+
+        drawerToggle.setOnMouseClicked(this::drawerAction);
+        menu.setOnMouseClicked(this::routerGoto);
+        tables.setOnMouseClicked(this::routerGoto);
+        commands.setOnMouseClicked(this::routerGoto);
+        employees.setOnMouseClicked(this::routerGoto);
+        report.setOnMouseClicked(this::routerGoto);
+    }
+
+    public void setFactoryController(FactoryController controller) {
+        this.factoryController = controller;
+    }
+
+    private void drawerAction(MouseEvent e) {
+        Button toggle = (Button) e.getSource();
+        State state = (State) toggle.getProperties().get("state");
+
+        if(state.equals(State.OPEN)) {
+            toggle.getProperties().put("state", State.CLOSED);
+            drawer.setMaxWidth(75);
+            return;
+        }
+
+        if(state.equals(State.CLOSED)) {
+            toggle.getProperties().put("state", State.OPEN);
+            drawer.setMaxWidth(1.7976931348623157E308);
+        }
+    }
+
+    private void routerGoto(MouseEvent e) {
+        HBox menuOption = (HBox) e.getSource();
+        Target target = (Target) menuOption.getProperties().get("target");
+
+        if(target.equals(Target.MENU)) {
+            factoryController.setIncludedFXML("views/page1.fxml");
+            return;
+        }
+
+        if(target.equals(Target.TABLES)) {
+            factoryController.setIncludedFXML("views/page2.fxml");
+            return;
+        }
+
+        if(target.equals(Target.COMMANDS)) {
+            factoryController.setIncludedFXML("views/page3.fxml");
+            return;
+        }
+
+        if(target.equals(Target.EMPLOYEES)) {
+            factoryController.setIncludedFXML("views/page4.fxml");
+            return;
+        }
+
+        if(target.equals(Target.REPORT)) {
+            factoryController.setIncludedFXML("views/page5.fxml");
+        }
+    }
+}
