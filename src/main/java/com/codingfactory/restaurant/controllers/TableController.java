62048cc42b0c33dafb29559a84496921d0154e32
Héloïse Le Lez <lelez.heloise@gmail.com>
2023-02-08 16:18:07 +0100

    Tablecontroller test mongodb

--- src/main/java/com/codingfactory/restaurant/controllers/TableController.java
+++ src/main/java/com/codingfactory/restaurant/controllers/TableController.java
@@ -1,4 +1,13 @@
 package com.codingfactory.restaurant.controllers;
 
+import com.codingfactory.restaurant.MongoConnection;
+import com.mongodb.client.MongoDatabase;
+
+
 public class TableController {
+    public static void main(String[] args) {
+        MongoDatabase db = MongoConnection.getDatabase();
+        db.getCollection("tables").find();
+
+    }
 }
